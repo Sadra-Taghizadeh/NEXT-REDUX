17520cbfe1233f9e03a4f727f0d895cdaf981ec0
Sadra_Taghizadeh <110200987+Sadra-Taghizadeh@users.noreply.github.com>
2023-07-13 16:31:56 +0330

    fix

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Login from "@components/Login";
 import Profile from "@components/Profile";
 import ChangeColor from "@components/ChangeColor";
 import { Provider } from "react-redux";
-import store from "./store";
+import store from "../components/store";
 const Home = () => {
   return (<div>
     <Provider store={store}>
---
+++ components/store.jsx
@@ -0,0 +1,8 @@
+"use client"
+
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
+
+export default store;
